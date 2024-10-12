ad084bf409d04314acf8c0f760dfc8a75cfbc6c6
NAGARAJS2002 <nr2277644@gmail.com>
2024-10-12 11:38:06 +0530

    connected mongoDB

--- api/index.js
+++ api/index.js
@@ -1,9 +1,15 @@
 
 import express from "express";
+import mongoose from "mongoose";
+import env from "dotenv"
+env.config();
 const app = express();
 const port = 3000;
 
-
+mongoose.connect(process.env.MONGODB_URL).then(() => {
+    console.log('database connected');
+    
+})
 
 
 
