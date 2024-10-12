cca293adc9bc04a886de1075e38a2404bb737a02
NAGARAJS2002 <nr2277644@gmail.com>
2024-10-12 11:47:06 +0530

    create router api

--- api/index.js
+++ api/index.js
@@ -1,7 +1,9 @@
 
 import express from "express";
 import mongoose from "mongoose";
-import env from "dotenv"
+import env from "dotenv";
+import userRouter from "./routes/userRoute.js"
+import authRouter from "./routes/userRoute.js"
 env.config();
 const app = express();
 const port = 3000;
@@ -16,4 +18,8 @@ mongoose.connect(process.env.MONGODB_URL).then(() => {
 app.listen(port,() => {
     console.log(`server is running on port ${port}`);
 
-})
+});
+
+
+app.use('/api/auth/',authRouter);
+app.use('/api/user/',userRouter);
