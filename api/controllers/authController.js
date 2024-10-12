c0ec08a2cebbbc0ac95d8e14465916a10d74015c
NAGARAJS2002 <nr2277644@gmail.com>
2024-10-12 16:44:45 +0530

    created signin api route

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,6 +1,6 @@
 import bcryptjs from "bcryptjs";
 import User from  "../model/userModel.js"
-
+import jwt from "jsonwebtoken"
 export const signup = async  (req,res,next) =>{
     const {username,email,password} = req.body;
     if(!username ||!email || !password) {
@@ -26,4 +26,21 @@ export const signup = async  (req,res,next) =>{
     } catch (error) {
        next(error) 
     }
+};
+
+export const signin = async  (req,res,next) => {
+    const {email ,password} = req.body;
+   try {
+      const validUser = await User.findOne({email})
+      if (!validUser) return next(errorHandler(404,`user not found `));
+
+     const validPassword = bcryptjs.compareSync(password,validUser.password);
+     if(!validPassword) return next(errorHandler(401,'wrong credential!'));
+
+     const token =  jwt.sign({ id: validUser._id},process.env.JWT_SECRET)
+     const {password:pass,...rest} = validUser._doc;
+     res.cookie('access_token',token,{http:true}).status(201).json(rest)
+   } catch (error) {
+    next(error)
+   }
 }
